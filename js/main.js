ae427664d83263257ccdc74048d0d6329c6daea5
Ceroick <68435872+Ceroick@users.noreply.github.com>
2026-05-27 20:53:06 -0500

    Add dispararTos function to initiate stimulus sequence

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+function dispararTos(tipoEstimulo) {
+    console.log("Iniciando secuencia para: " + tipoEstimulo);
+    
+    // 1. Mostrar efecto visual del estímulo (ej. humo entrando)
+    // 2. Ejecutar la función del Bloque Motor (los 6 pasos)
+    animarFaseAferente(tipoEstimulo);
+    setTimeout(animarBloqueMotor, 1000); 
+}
